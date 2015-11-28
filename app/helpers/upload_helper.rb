edac410ad41e3ab6d4f8fa19766a7eb3231a2927
Amr Alaa <n1amr1@gmail.com>
2015-11-28 08:19:39 +0200

    change mime type for text files

--- app/helpers/upload_helper.rb
+++ app/helpers/upload_helper.rb
@@ -2,13 +2,15 @@ require File.join(Rails.root,'lib/google/files')
 require File.join(Rails.root,'lib/google/api')
 
 module UploadHelper
-    def self.upload_file(upload)
+    def self.upload_file(upload, mime_type = "image/jpg")
         fman = GDrive::FileManager.new()
 
         title = upload[:dataFile].original_filename
         dir = Dir.new('public/data/')
         path = File.join('public/data/', title)
         File.open(path, "wb") { |f| f.write(upload[:dataFile].read) }
-        fman.upload_file(path, title, title, "image/jpg", nil)
+
+        mime_type = "text/plain" if /^.*\.t[e]*xt/ === title
+        fman.upload_file(path, title, title, mime_type, nil)
     end
 end
